f9a7efed3cbb693264038fd9d36b2f9695378438
Brian Kiprotich <briankiprotich602@gmail.com>
2026-04-08 12:38:17 +0300

    Creating a login tracker

--- index.js
+++ index.js
@@ -4,3 +4,19 @@
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
 };
+
+function createLoginTracker(userInfo){
+  const {userName, password} = userInfo;
+  let attemptCount = 0;
+  return (passwordAttempt) => {
+    attemptCount += 1;
+    if (passwordAttempt === password && attemptCount < 3){
+      return "Login successful"
+    } else if (passwordAttempt !== password && attemptCount <= 3){
+        return `Attempt ${attemptCount}: Login failed`
+    } else{
+        return "Account locked due to too many failed login attempts"
+    }
+  }
+
+}
